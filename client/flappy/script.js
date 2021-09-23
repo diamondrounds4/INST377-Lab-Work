bacdb939e7197db4b39dbe8f9fd441927cc20444
diamondrounds4 <drounds2@umd.edu>
2021-09-22 23:24:12 -0400

    Update script.js

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded' , () => {
             }
             if (
                 obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
-                birdBottom < obstacleBottom + 155||
+                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap - 200)||
                 birdBottom === 0
                 ) {
                 gameOver()

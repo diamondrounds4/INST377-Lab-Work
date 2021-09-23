e524ac9d7c2f1d7c32c5f92ab061fc985e0880fd
diamondrounds4 <drounds2@umd.edu>
2021-09-22 21:35:15 -0400

    ...

--- client/flappy/script.js
+++ client/flappy/script.js
@@ -10,10 +10,10 @@ document.addEventListener('DOMContentLoaded' , () => {
     function startGame() {
         birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
-        bird.style.left = birdLeft + 'px'
+        bir.style.left = birdLeft + 'px'
     }
 
-    let gameTimerId = setInterval(startGame, 20)
+    let timerId = setInterval(startGame, 20)
 
     function control(e) {
         if (e.keyCode === 32) {

c4726dd45f9fbe59fe51788f986726c5bad2b574
diamondrounds4 <drounds2@umd.edu>
2021-09-26 22:52:43 -0400

    Update script.js

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,41 +1,43 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('carousel_item');
-const totalSlides = slides.length
+const slides = document.querySelectorAll('carousel__item');
+const totalSlides = slides.length;
 
 document.
-    querySelector('carousel_next')
-    .addEventListener('click', function() {
-        nextSlide();
+    querySelector('carousel__button--next')
+    .addEventListener("click", function() {
+    nextSlide();
     });
-
 document.
-    querySelector('carousel_prev')
-    .addEventListener('click', function() {
-        prevSlide();
+    querySelector('carousel__button--prev')
+    .addEventListener("click", function() {
+    prevSlidee();
     });
 
 function updateSlidePosition() {
     for (let slide of slides) {
-        slide.classList.remove('carousel_item--visible');
-        slide.classList.add('carousel_item--hidden');
-    }
-    slides[slidePosition].classList.add('carousel_item--visible')
+    slide.classList.remove('carousel__item--visible');
+    slide.classList.add('carousel__item--hidden');
+     }
+
+    slides[slidePosition].classList.add('carousel__item--visible');
 }
 
 function nextSlide() {
-    if (slidePosition == totalSlides - 1) {
+    if (slidePosition === totalSlides - 1) {
         slidePosition = 0;
     } else {
         slidePosition++;
     }
+
     updateSlidePosition();
 }
 
 function prevSlide() {
-    if (slidePosition == totalSlides) {
-        slidePosition =  totalSlides - 1;
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
     } else {
         slidePosition--;
     }
+
     updateSlidePosition();
 }

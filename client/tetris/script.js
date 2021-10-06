e8ff43d14bfcb47a2e247bf14be346d481f7de2d
diamondrounds4 <drounds2@umd.edu>
2021-10-06 16:45:23 -0400

    fixing errors

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -94,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
       currentPosition = 4
       draw()
       displayShape()
-      // addScore()
+      addScore()
     }
   }
 
@@ -164,19 +164,20 @@ document.addEventListener('DOMContentLoaded', () => {
       displayShape()
     }
   })
-})
-// function addScore() {
-// for (let i = 0; i < 199; i += width) {
-// const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
 
-// if (row.every(index => squares[index].classList.contains('taken'))) {
-// score += 10
-// scoreDisplay.innerHTML = score
-// row.forEach(index => {
-// squares[index].classList.remove('taken')
-// })
-// const squaresRemoved = squares.splice(i, width)
-// console.log(squaresRemoved)
-// }
-// }
-// }
+  function addScore() {
+    for (let i = 0; i < 199; i += width) {
+      const row = [i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7, i + 8, i + 9]
+
+      if (row.every(index => squares[index].classList.contains('taken'))) {
+        score += 10
+        scoreDisplay.innerHTML = score
+        row.forEach(index => {
+          squares[index].classList.remove('taken')
+        })
+        const squaresRemoved = squares.splice(i, width)
+        console.log(squaresRemoved)
+      }
+    }
+  }
+})

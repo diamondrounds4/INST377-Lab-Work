07393aee59f4cd05b761683327f6bce9249cd4a9
diamondrounds4 <drounds2@umd.edu>
2021-10-06 16:06:46 -0400

    Update script.js

--- client/tetris/script.js
+++ client/tetris/script.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
     } else if (e.keyCode === 39) {
       moveRight()
     } else if (e.keyCode === 40) {
-      moveDown()
+      //moveDown()
     }
   }
   document.addEventListener('keyup', control)
